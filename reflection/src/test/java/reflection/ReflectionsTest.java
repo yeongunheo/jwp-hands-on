3d58a328b8d8d900dd8bc0c0a5a02886775eff80
yeongunheo <gjduddns@gmail.com>
2022-09-17 11:53:22 +0900

    feat: '자바 리플렉션 사용하기' 요구사항 구현

--- reflection/src/test/java/reflection/ReflectionsTest.java
+++ reflection/src/test/java/reflection/ReflectionsTest.java
@@ -1,5 +1,9 @@
 package reflection;
 
+import annotation.Controller;
+import annotation.Repository;
+import annotation.Service;
+import java.util.Set;
 import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
@@ -14,5 +18,17 @@ class ReflectionsTest {
         Reflections reflections = new Reflections("examples");
 
         // TODO 클래스 레벨에 @Controller, @Service, @Repository 애노테이션이 설정되어 모든 클래스 찾아 로그로 출력한다.
+        Set<Class<?>> list1 = reflections.getTypesAnnotatedWith(Controller.class);
+        for (Class<?> clazz : list1) {
+            log.info(clazz.getName());
+        }
+        Set<Class<?>> list2 = reflections.getTypesAnnotatedWith(Service.class);
+        for (Class<?> clazz : list2) {
+            log.info(clazz.getName());
+        }
+        Set<Class<?>> list3 = reflections.getTypesAnnotatedWith(Repository.class);
+        for (Class<?> clazz : list3) {
+            log.info(clazz.getName());
+        }
     }
 }
